f085b5bd0b6957927ee1680d700ce30c6d598214
Jessica <jessicaesimpson91@gmail.com>
2017-07-03 19:26:07 -0500

    first commit

---
+++ rubyPuzzles.rb
@@ -0,0 +1,58 @@
+#arr = [3,5,1,2,7,9,8,13,25,32]
+#puts arr.reduce(:+)
+#puts arr.each.reject{ |x| x < 10}
+
+#arr = ['John', 'KB', 'Oliver', 'Cory', 'Matthew', 'Christopher']
+#puts arr.shuffle
+#puts arr.each.reject { |x| x.length < 5}
+
+#arr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+#arr.shuffle!
+#puts arr.last
+#puts arr.first
+#if arr.first == 'a' || arr.first == 'e' ||arr.first == 'i' ||arr.first == 'o' || arr.first =='u'
+    #puts "The first letter in the array is a vowel"
+#end     
+
+#arr = []
+#i = 0 
+#while i < 10
+    #arr.push(rand(55..100))
+    #i += 1
+#end     
+#puts arr.inspect 
+
+#arr = []
+#i = 0 
+#while i < 10
+    #arr.push(rand(55..100))
+    #i += 1
+#end     
+#puts arr.sort.inspect 
+#puts arr.min
+#puts arr.max
+
+#str = ""
+#i = 0
+#while i < 5
+    #str.concat((65+rand(26)).chr)
+    #i += 1
+#end
+#puts str    
+
+
+#arr = []
+#x = 0
+#while x < 10
+    #i = 0
+    #str = ""
+    #while i < 5
+        #str.concat((65+rand(26)).chr)
+        #i += 1
+    #end
+    #arr.push(str) 
+    #x += 1
+#end 
+#puts arr.inspect
+
+ 
